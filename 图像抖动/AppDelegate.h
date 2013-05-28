1fecf5f4f624a448bfd6a04791b786658637fb6d
long <long@longtekiMac-mini.local>
2013-05-28 17:16:47 +0800

    Initial Commit

---
+++ 图像抖动/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  图像抖动
+//
+//  Created by long on 13-5-28.
+//  Copyright (c) 2013年 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 图像抖动/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  图像抖动
+//
+//  Created by long on 13-5-28.
+//  Copyright (c) 2013年 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
